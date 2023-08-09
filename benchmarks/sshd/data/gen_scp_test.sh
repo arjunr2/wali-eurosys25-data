456cd38b0f8d92331b41994f53a587def25c69be
tc <thetianshuhuang@gmail.com>
2023-08-09 19:28:09 +0000

    Added sshd code and dirgen script

--- benchmarks/sshd/data/gen_scp_test.sh
+++ benchmarks/sshd/data/gen_scp_test.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
 
-base_size=262144 #KB
-cur_dir=""
+steps=16
+step_dir="sdir_$steps/"
 
-while [ $base_size -ge 16 ]
+while [ $steps -ge 1 ]
 do
-  echo $base_size
-  cur_dir+="tdir_$base_size/"
-  mkdir -p $cur_dir
-  for (( c=0; c<=$(($base_size/16)); c++))
-  do
-    head -c 16K </dev/urandom > $cur_dir/tfile_$c
-  done
-  base_size=$(($base_size/2))
+	mkdir -p $step_dir
+	base_size=16 #KB
+	file_size=1 #KB
+	cur_dir="$step_dir"
+
+	while [ $base_size -ge 1 ]
+	do
+	  echo $base_size
+	  cur_dir+="tdir/"
+	  mkdir -p $cur_dir
+	  for (( c=0; c<$((($base_size/2)*($file_size))); c++))
+	  do
+	    head -c ${file_size}K </dev/urandom > $cur_dir/tfile_$c
+	  done
+	  base_size=$(($base_size/2))
+	done
+
+	steps=$(($steps-1))
+	step_dir+="sdir_$steps/"
 done
+
 
