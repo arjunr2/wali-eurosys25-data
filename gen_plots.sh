9e5f91786e576dfafa6dd3df905679fdf54e461f
Arjun Ramesh <arjunr2@andrew.cmu.edu>
2025-02-06 19:10:26 +0000

    Tar all the data

--- gen_plots.sh
+++ gen_plots.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+mkdir -p figures
 scripts=("scatter.py" "sigpoll.py" "syscall_archs.py" "syscall_overhead.py" "syscall_profile.py" "wali_macrobench.py")
 for i in ${!scripts[@]}; do
 	script=${scripts[$i]}
